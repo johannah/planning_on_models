19d9755dd27893f1c87fa1f26ad4fb58da312f07
johannah <jh1736@gmail.com>
2019-02-02 22:42:10 -0500

    trying multi heads but not training

--- agents/bootstrap_ddqn_atari.py
+++ agents/bootstrap_ddqn_atari.py
@@ -45,6 +45,7 @@ def train_batch(batch, cnt):
     next_q_values = policy_net_ensemble(nexts_pt, None)
     next_q_state_values = target_net_ensemble(nexts_pt, None)
 
+    cnt_losses = []
     for k in range(info['N_ENSEMBLE']):
         # TODO mask
         total_used = torch.sum(mask_pt[:, k])
@@ -55,9 +56,16 @@ def train_batch(batch, cnt):
             full_loss = (q_value-expected_q_value.detach()).pow(2).mean()
             full_loss = mask_pt[:,k]*full_loss
             loss = torch.sum(full_loss/total_used)
-            loss.backward(retain_graph=True)
-            opt.step()
-            losses[k] = loss.cpu().detach().item()
+            cnt_losses.append(loss)
+            #loss.backward(retain_graph=True)
+            #losses[k] = loss.cpu().detach().item()
+    all_loss = torch.stack(cnt_losses).sum()
+    all_loss.backward()
+    #for param in policy_net_ensemble.parameters():
+    #    if param.grad is not None:
+    #        param.grad.data *=1.0/info['N_ENSEMBLE']
+    #torch.nn.utils.clip_grad_value_(policy_net_ensemble.parameters(), info['CLIP_GRAD'])
+    opt.step()
     if not cnt%info['TARGET_UPDATE']:
         print("++++++++++++++++++++++++++++++++++++++++++++++++")
         print('updating target network')
@@ -72,8 +80,8 @@ def handle_checkpoint(last_save, cnt, epoch):
                  'optimizer':opt.state_dict(),
                  'cnt':cnt,
                  'epoch':epoch,
-                 'policy_net_state_dict':policy_net.state_dict(),
-                 'target_net_state_dict':target_net.state_dict(),
+                 'policy_net_ensemble_state_dict':policy_net_ensemble.state_dict(),
+                 'target_net_ensemble_state_dict':target_net_ensemble.state_dict(),
                  }
         filename = os.path.abspath(model_base_filepath + "_%010dq.pkl"%cnt)
         save_checkpoint(state, filename)
@@ -94,6 +102,7 @@ def handle_step(cnt, S_hist, S_prime, action, reward, finished, k_used, acts, ep
     return cnt, S_hist, batch, episodic_reward
 
 def run_training_episode(epoch_num, total_steps, last_save):
+    epsilon = 0.0
     start = time.time()
     episodic_losses = []
     start_steps = total_steps
@@ -125,10 +134,11 @@ def run_training_episode(epoch_num, total_steps, last_save):
             #vals = [q.cpu().data.numpy() for q in policy_net(S_hist_pt, None)]
             #acts = [np.argmax(v, axis=-1)[0] for v in vals]
 
-        epsilon = epsilon_by_frame(total_steps)
-        if (random_state.rand() < epsilon):
-            action = random_state.choice(action_space)
-            #k_used = info['RANDOM_HEAD']
+        if info['USE_EPSILON']:
+            epsilon = epsilon_by_frame(total_steps)
+            if (random_state.rand() < epsilon):
+                action = random_state.choice(action_space)
+            k_used = info['RANDOM_HEAD']
         #else:
         #    action = acts[active_head]
             #k_used = active_head
@@ -151,12 +161,14 @@ def run_training_episode(epoch_num, total_steps, last_save):
     stop = time.time()
     ep_time =  stop - start
     board_logger.scalar_summary('Reward per episode', epoch_num, episodic_reward)
+    board_logger.scalar_summary('head per episode', epoch_num, active_head)
     board_logger.scalar_summary('Avg get batch time', epoch_num, np.mean(checkpoint_times))
     board_logger.scalar_summary('epoch time', epoch_num, ep_time)
+    board_logger.scalar_summary('epoch steps', epoch_num, total_steps-start_steps)
     print("EPISODE:%s HEAD %s REWARD:%s ------ ep %04d total %010d steps"%(epoch_num, active_head, episodic_reward, total_steps-start_steps, total_steps))
     #print('actions',episode_actions)
     print("time for episode", ep_time)
-    return episodic_reward, total_steps, ep_time, last_save, np.mean(episodic_losses)
+    return active_head, episodic_reward, total_steps, ep_time, last_save, np.mean(episodic_losses)
 
 def write_info_file(cnt):
     info_filename = model_base_filepath + "_%010d_info.txt"%cnt
@@ -185,16 +197,16 @@ if __name__ == '__main__':
     # 1 is fire
     # 0 is noop
     info = {
-        "GAME":'Pong', # gym prefix
+        'USE_EPSILON':False,
+        "GAME":'Breakout', # gym prefix
         "DEVICE":device,
-        "NAME":'_Pong_BSddqn', # start files with name
-        "N_ENSEMBLE":1, # number of heads to use
-        "N_EVALUATIONS":10, # Number of evaluation episodes to run
+        "NAME":'_Pong', # start files with name
+        "N_ENSEMBLE":7, # number of heads to use
         "BERNOULLI_P": 1.0, # Probability of experience to go to each head
-        "TARGET_UPDATE":1000, # TARGET_UPDATE how often to use replica target
+        "TARGET_UPDATE":50000, # TARGET_UPDATE how often to use replica target
         "USE_DOUBLE_DQN":False, # Whether to use double DQN or regular DQN
-        "CHECKPOINT_EVERY_STEPS":10000,
-        "ADAM_LEARNING_RATE": 1e-4,
+        "CHECKPOINT_EVERY_STEPS":30000,
+        "ADAM_LEARNING_RATE": .00025,
         "CLIP_REWARD_MAX":1,
         "CLIP_REWARD_MAX":-1,
         "HISTORY_SIZE":4, # how many past frames to use for state input
@@ -213,6 +225,9 @@ if __name__ == '__main__':
         "NETWORK_INPUT_SIZE":(84,84),
         }
 
+    if not info['USE_EPSILON'] and info['N_ENSEMBLE'] == 1:
+        print('no epsilon and single epsilon')
+        embed()
     info['FAKE_ACTS'] = [info['RANDOM_HEAD'] for x in range(info['N_ENSEMBLE'])]
     info['args'] = args
     accumulation_rewards = []
@@ -265,8 +280,8 @@ if __name__ == '__main__':
     if args.model_loadpath is not '':
         # what about random states - they will be wrong now???
         # TODO - what about target net update cnt
-        target_net_ensemble.load_state_dict(model_dict['target_net_state_dict'])
-        policy_net_ensemble.load_state_dict(model_dict['policy_net_state_dict'])
+        target_net_ensemble.load_state_dict(model_dict['target_net_ensemble_state_dict'])
+        policy_net_ensemble.load_state_dict(model_dict['policy_net_ensemble_state_dict'])
         opt.load_state_dict(model_dict['optimizer'])
         print("loaded model state_dicts")
         if args.buffer_loadpath == '':
@@ -287,11 +302,18 @@ if __name__ == '__main__':
     all_rewards = []
 
     epsilon_by_frame = lambda frame_idx: info['EPSILON_MIN'] + (info['EPSILON_MAX'] - info['EPSILON_MIN']) * math.exp( -1. * frame_idx / info['EPSILON_DECAY'])
+    total_head_reward = [0.0 for x in range(info['N_ENSEMBLE'])]
+    head_count = [0 for x in range(info['N_ENSEMBLE'])]
     for epoch_num in range(epoch_start, info['N_EPOCHS']):
-        ep_reward, total_steps, etime, last_save, mean_loss = run_training_episode(epoch_num, total_steps, last_save)
+        active_head, ep_reward, total_steps, etime, last_save, mean_loss = run_training_episode(epoch_num, total_steps, last_save)
+        head_count[active_head] +=1
+        total_head_reward[active_head]+=ep_reward
+        avg_by_head = [total_head_reward[k]/(head_count[k]+.01) for k in range(info['N_ENSEMBLE'])]
         all_rewards.append(ep_reward)
         overall_time += etime
         board_logger.scalar_summary("avg reward last 100 episodes", epoch_num, np.mean(all_rewards[-100:]))
+        print("HEAD AVERAGE")
+        print(avg_by_head)
 
 #        if (total_steps - last_target_update) >= info['TARGET_UPDATE']:
 #            print("Updating target network at {}".format(epoch_num))
