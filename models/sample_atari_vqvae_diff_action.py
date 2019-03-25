02c67da06ca2ab4dd1a819f375cdcc11dd6e74dc
johannah <jh1736@gmail.com>
2019-03-25 15:37:49 -0400

    added mse

--- models/sample_atari_vqvae_diff_action.py
+++ models/sample_atari_vqvae_diff_action.py
@@ -23,15 +23,18 @@ def sample_batch(data, episode_number, episode_reward, name):
         x = (2*reshape_input(states)-1).to(DEVICE)
         diff = (reshape_input(pred_states[:,1][:,None])).to(DEVICE)
         prev_true = (reshape_input(states[:,-2:-1])*255).cpu().numpy().astype(np.int)
-        if not args.reward_int:
-            true_signals = values.cpu().numpy()
-        else:
+
+        if args.reward_int:
             true_signals = rewards.cpu().numpy()
+        else:
+            true_signals = values.cpu().numpy()
+
         action_preds = []
         action_preds_lsm = []
         action_preds_wrong = []
         action_steps = []
         action_trues = []
+        signal_preds = []
         # (args.nr_logistic_mix/2)*3 is needed for each reconstruction
         for i in range(states.shape[0]):
             x_d, z_e_x, z_q_x, latents, pred_actions, pred_signals = vqvae_model(x[i:i+1])
@@ -44,10 +47,16 @@ def sample_batch(data, episode_number, episode_reward, name):
             diff_true = diff[i,0]
 
             if args.reward_int:
+                print('using int reward')
                 pred_signal = torch.argmax(pred_signals).item()
+            elif 'num_rewards' in info.keys():
+                print('using val reward')
+                pred_signal = (pred_signals[0].cpu().numpy()).astype(np.int)
             else:
-                pred_signal = pred_signals[0].cpu().numpy()
+                print('using no reward')
+                pred_signal = -99
 
+            signal_preds.append(pred_signal)
             f,ax = plt.subplots(2,3)
             title = 'step %s/%s action %s reward %s' %(i, states.shape[0], actions[i].item(), rewards[i].item())
             pred_action = torch.argmax(pred_actions).item()
@@ -62,30 +71,22 @@ def sample_batch(data, episode_number, episode_reward, name):
             print("A",action_preds_lsm[-1], pred_action, action)
             print("R",true_signals[i], pred_signal)
             iname = os.path.join(output_savepath, '%s_E%05d_R%03d_%05d.png'%(name, int(episode_number), int(episode_reward), i))
-            if not os.path.exists(iname):
-                ax[0,0].imshow(prev_true[i,0])
-                ax[0,0].set_title('prev TA:%s PR:%s'%(action,pred_action))
-                ax[1,0].set_title('est  TA:%s PR:%s'%(action,pred_action))
-                ax[0,1].imshow(rec_true, vmin=0, vmax=255)
-                ax[0,1].set_title('rec true TR:%s PR:%s'%(true_signals[i], pred_signal))
-                ax[1,1].imshow(rec_est, vmin=0, vmax=255)
-                ax[1,1].set_title('rec est  TR:%s PR:%s'%(true_signals[i], pred_signal))
-                ax[0,2].imshow(diff_true, vmin=-1, vmax=1)
-                ax[0,2].set_title('diff true')
-                ax[1,2].imshow(diff_est, vmin=-1, vmax=1)
-                ax[1,2].set_title('diff est')
-                plt.suptitle(title)
-                plt.savefig(iname)
-                plt.close()
-                if not i%10:
-                    print("saving", os.path.split(iname)[1])
-
-        gif_path = iname[:-10:] + '.gif'
-        if not os.path.exists(gif_path):
-            search_path = iname[:-10:] + '*.png'
-            cmd = 'convert %s %s' %(search_path, gif_path)
-            print('creating gif', gif_path)
-            os.system(cmd)
+            ax[0,0].imshow(prev_true[i,0])
+            ax[0,0].set_title('prev TA:%s PR:%s'%(action,pred_action))
+            ax[1,0].set_title('est  TA:%s PR:%s'%(action,pred_action))
+            ax[0,1].imshow(rec_true, vmin=0, vmax=255)
+            ax[0,1].set_title('rec true TR:%s PR:%s'%(round(true_signals[i],2),round(pred_signal,2)))
+            ax[1,1].imshow(rec_est, vmin=0, vmax=255)
+            ax[1,1].set_title('rec est')
+            ax[0,2].imshow(diff_true, vmin=-1, vmax=1)
+            ax[0,2].set_title('diff true')
+            ax[1,2].imshow(diff_est, vmin=-1, vmax=1)
+            ax[1,2].set_title('diff est')
+            plt.suptitle(title)
+            plt.savefig(iname)
+            plt.close()
+            if not i%10:
+                print("saving", os.path.split(iname)[1])
         # plot actions
         aname = os.path.join(output_savepath, '%s_E%05d_action.png'%(name, int(episode_number)))
         plt.figure()
@@ -113,7 +114,34 @@ def sample_batch(data, episode_number, episode_reward, name):
             print(v)
             af.write(v+'\n')
         af.close()
-        embed()
+
+        srname = os.path.join(output_savepath, '%s_E%05d_signal.txt'%(name, int(episode_number)))
+        sf = open(srname, 'w')
+        if 'num_rewards' in info.keys():
+            signal_preds = np.array(signal_preds).astype(np.int)
+            signal_correct = []
+            signal_incorrect = []
+            signal_error = []
+
+            for s in sorted(list(set(true_signals))):
+                sigcor = np.sum(signal_preds[true_signals==s] ==  true_signals[true_signals==s])
+                sigicor = np.sum(signal_preds[true_signals==s] != true_signals[true_signals==s])
+                error = sigicor/float(np.sum(sigcor+sigicor))
+                signal_correct.append(sigcor)
+                signal_incorrect.append(sigicor)
+                signal_error.append(error)
+                v = 'reward signal {} correct {} incorrect {} error {}'.format(s,sigcor,sigicor,error)
+                print(v)
+                sf.write(v+'\n')
+        else:
+            mse = np.square(signal_preds-true_signals).sum()
+            sf.write('mse: %s'%mse)
+        sf.close()
+        gif_path = iname[:-10:] + '.gif'
+        search_path = iname[:-10:] + '*.png'
+        cmd = 'convert %s %s' %(search_path, gif_path)
+        print('creating gif', gif_path)
+        os.system(cmd)
 
 if __name__ == '__main__':
     import argparse
@@ -174,24 +202,33 @@ if __name__ == '__main__':
     wsize = valid_data_loader.data_w
 
     if args.reward_int:
+        int_reward = info['num_rewards']
         vqvae_model = VQVAE(num_clusters=largs.num_k,
                             encoder_output_size=largs.num_z,
                             num_output_mixtures=info['num_output_mixtures'],
                             in_channels_size=largs.number_condition,
                             n_actions=info['num_actions'],
                             int_reward=info['num_rewards']).to(DEVICE)
-    else:
+    elif 'num_rewards' in info.keys():
         vqvae_model = VQVAE(num_clusters=largs.num_k,
                             encoder_output_size=largs.num_z,
                             num_output_mixtures=info['num_output_mixtures'],
                             in_channels_size=largs.number_condition,
                             n_actions=info['num_actions'],
-                            reward_value=largs.reward_value).to(DEVICE)
+                            reward_value=True).to(DEVICE)
+    else:
+       vqvae_model = VQVAE(num_clusters=largs.num_k,
+                            encoder_output_size=largs.num_z,
+                            num_output_mixtures=info['num_output_mixtures'],
+                            in_channels_size=largs.number_condition,
+                            n_actions=info['num_actions'],
+                            ).to(DEVICE)
 
     vqvae_model.load_state_dict(model_dict['vqvae_state_dict'])
     #valid_data, valid_label, test_batch_index = data_loader.validation_ordered_batch()
-    #valid_episode_batch, episode_index, episode_reward = valid_data_loader.get_entire_episode(diff=True)
-    #sample_batch(valid_episode_batch, episode_index, episode_reward, 'valid')
+    valid_episode_batch, episode_index, episode_reward = valid_data_loader.get_entire_episode(diff=True)
+    sample_batch(valid_episode_batch, episode_index, episode_reward, 'valid')
+
     train_episode_batch, episode_index, episode_reward = train_data_loader.get_entire_episode(diff=True)
     sample_batch(train_episode_batch, episode_index, episode_reward, 'train')
 
